ddbcf763482fcae1d3c2ab45cd5e55bb59407d77
ZordonLord <zordonlord@mail.ru>
2026-02-16 23:41:55 +1000

    Добавлена главная страница и конфиги

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/env.php';
+
+loadEnv(__DIR__ . '/.env');
+
+return [
+    'clientId'     => $_ENV['AMO_CLIENT_ID'],
+    'clientSecret' => $_ENV['AMO_CLIENT_SECRET'],
+    'redirectUri'  => $_ENV['AMO_REDIRECT_URI'],
+    'baseDomain'   => $_ENV['AMO_BASE_DOMAIN'],
+];
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$config = require __DIR__ . '/config.php';
+$storageFile = __DIR__ . '/storage/tokens.json';
+$isAuthorized = file_exists($storageFile);
+
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>OAuth amoCRM</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Авторизация amoCRM</h2>
+
+        <?php if ($isAuthorized): ?>
+            <a href="callback.php?logout=1" class="btn">Выйти</a>
+            <br /><br />
+            <a href="callback.php" class="btn">Перейти к callback странице</a>
+        <?php else: ?>
+            <script
+                class="amocrm_oauth"
+                charset="utf-8"
+                data-client-id="<?= $config['clientId'] ?>"
+                data-title="Авторизоваться через amoCRM"
+                data-compact="false"
+                data-class-name="className"
+                data-color="default"
+                data-state="state"
+                data-error-callback="functionName"
+                data-mode="popup"
+                src="https://www.amocrm.ru/auth/button.min.js"></script>
+            <br /><br />
+            <a href='callback.php' class="btn">Перейти к callback странице</a>
+        <?php endif; ?>
+    </div>
+</body>
+</html>
