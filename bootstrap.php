5b21954efa1a837d4cf7412480d43e64862b00de
ZordonLord <zordonlord@mail.ru>
2026-02-22 17:19:53 +1000

    Добавлена функция логирования ошибок

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/OAuthClient.php';
+require_once __DIR__ . '/logger.php';
 $config = require __DIR__ . '/config.php';
 
 // Создаём и получаем экземпляр OAuthClient / config / storageFile
---
+++ logger.php
@@ -0,0 +1,16 @@
+<?php
+// Функция для логирования ошибок в файл storage/error.log
+function log_error(string $message, array $context = []): void
+{
+    $file = __DIR__ . '/storage/error.log';
+
+    $time = date('Y-m-d H:i:s');
+
+    if (!empty($context)) {
+        $message .= ' | ' . json_encode($context, JSON_UNESCAPED_UNICODE);
+    }
+
+    $line = "[$time] $message" . PHP_EOL;
+
+    file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+}
